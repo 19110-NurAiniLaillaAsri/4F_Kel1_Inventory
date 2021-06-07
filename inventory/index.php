1b62426a33095634d14a0b2e8821434e2475dfb1
19110-NurAiniLaillaAsri <83807811+19110-NurAiniLaillaAsri@users.noreply.github.com>
2021-06-07 18:39:52 +0700

    Tambah css

--- inventory/index.php
+++ inventory/index.php
@@ -6,64 +6,49 @@ if (!isset($_SESSION['log'])) {
 } else {
 	header('location:stok.php');
 }
-
-
  ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-	<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-	<title>Login</title>
-
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
-    <!-- Our Custom CSS -->
-    <link rel="stylesheet" href="css/inventory.css">
-
-    <!-- Font Awesome JS -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/login.css">
 </head>
 <body>
-
-	<div class="container col-md-6 mb-3">
-		<div class="row content">
-			<div class="col-md-12">
-			<div class="card-header">
-				<h3>Sign In</h3>
+	<div class="container fadeIn firs d-flex wrapper">
+		<div class="row content w-75 m-auto">
+			<div class="col-md-6 m-auto">
+				<img src="img/gudang5.png" class="img-fluid" alt="image">
 			</div>
-				<div class="card-body">
+			<div class="col-md-5 m-auto">
+				<div class="text-center">
+					<h3 class="signin-text mb-3">Sign In</h3>
+					<div class="underline-title"></div>
+				</div>
 				<form method="POST">
-					<div class="form-group">
+					<div class="form-group mt-2">
 						<label for="id_user">ID User</label>
 						<input type="id_user" name="id_user" class="form-control">
 					</div>
-					<div class="form-group">
+					<div class="form-group mt-2">
 						<label for="password">Password</label>
 						<input type="password" name="password" class="form-control">
 					</div>
-					<br>
-					<button class="btn btn-primary" name="login">Login
-					</button>
-					<br>
-					<br>
 					<div class="text-center">
-						<div class="small"><a href="register.php">Need an account? Sign up</a></div>
+						<input id="submit-btn" type="submit" name="login" value="LOGIN" /><br><a href="register.php" id="signup">Don't have account yet?</a>
 					</div>
 				</form>
-				</div>
 			</div>
 		</div>
 	</div>
 
-<script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
-
+ 	<script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="../js/bootstrap.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script> <!-- buat modal -->  
 </body>
 </html>
