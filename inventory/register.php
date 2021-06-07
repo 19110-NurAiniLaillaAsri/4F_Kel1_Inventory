bd0badaf85a3fcb87cf787bf9b5fd8efa3eaa541
19110-NurAiniLaillaAsri <83807811+19110-NurAiniLaillaAsri@users.noreply.github.com>
2021-06-07 18:41:33 +0700

    Update register.php

--- inventory/register.php
+++ inventory/register.php
@@ -5,84 +5,63 @@ if (!isset($_SESSION['log'])) {
 } else {
     header('location:stok.php');
 }
-
-
- ?>
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" href="css/login.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <title>Register</title>
-
-    <!-- Bootstrap CSS CDN -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
-    <!-- Our Custom CSS -->
-    <link rel="stylesheet" href="css/inventory.css">
-
-    <!-- Font Awesome JS -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <title>Registrasi Akun</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 </head>
 
 <body>
-<div class="container-fluid col-md-6 mb-3">
-    <div class="row content">
-        <div class="col-md-12">
-        <div class="card-header">
-            <h3>Sign Up</h3>
-            </div>
-            <div class="card-body">
-            <form method="POST">
-                <div class="form-group">
+<div class="container fadeIn firs d-flex wrapper">
+    <div class="row content w-75 m-auto">
+        <div class="text-center">
+            <h3>Sign Up</h3><br>
+            <div class="underline-title"></div>
+        </div>
+        <form method="POST">
+            <div class="col-md-6 m-auto">
+                <div class="form-group mt-2">
                     <label for="nama_user">Nama</label>
                     <input type="text" name="nama_user" class="form-control" required>
                 </div>
-                <div class="form-group">
+                <div class="form-group mt-2">
                     <label for="no_telp">No. Telepon</label>
                     <input type="text" name="no_telp" class="form-control" required>
                 </div>
-                <div class="form-group">
+                <div class="form-group mt-2">
                     <label for="email">Email</label>
                     <input type="email" name="email" class="form-control" required>
                 </div>
-                <div class="form-group">
-                    <label for="id_user">Username</label>
+                <div class="form-group mt-2">
+                    <label for="id_user">Id User</label>
                     <input type="text" name="id_user" class="form-control" required>
                 </div>
-                <div class="form-group">
+                <div class="form-group mt-2">
                     <label for="password">Password</label>
                     <input type="password" name="password" class="form-control" required>
                 </div>
-                <div class="form-group">
+                <div class="form-group mt-2">
                     <label for="password2">Konfirmasi Ulang Password</label>
                     <input type="password" name="password2" class="form-control" required>
                 </div>
-                <br>
-                <div class="form-group form-check">
-                    <input type="checkbox" name="checkbox" class="form-check-input" id="checkbox">
-                    <label class="form-check-label" for="checkbox">Pastikan data yang Anda isikan benar</label>
-                </div>
-                <br>
-                <button class="btn btn-primary" name="register">Registrasi Akun</button>
-                <br>
-                <br>
-                <div class="text-center">
-                    <div class="small"><a href="login.php">Have an account? Login</a></div>
-                </div>
-            </form>
             </div>
-        </div>
+            <div class="text-center">
+                <input id="submit-btn" type="submit" name="register" value="Registrasi Akun" /><br><a href="index.php" id="signup">Have an account? Login</a>
+            </div>
+        </form>    
     </div>
 </div>
 
-<script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
-
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="../js/bootstrap.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script> <!-- buat modal --> 
     </body>
 
 </html>
