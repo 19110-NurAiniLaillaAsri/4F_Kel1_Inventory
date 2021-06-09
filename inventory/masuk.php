10b3e5f9a2933c9ee0ce9afedae6cf477af14b07
19114-NurliAlifah <83756741+19114-NurliAlifah@users.noreply.github.com>
2021-06-09 14:30:53 +0700

    Update masuk.php

--- inventory/masuk.php
+++ inventory/masuk.php
@@ -1,6 +1,7 @@
 <?php 
 require 'function.php';
- ?>
+require 'cek.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -84,9 +85,7 @@ require 'function.php';
                                                 $jenisbarang = $fetcharray['nama_barang'];
                                                 $idbarang = $fetcharray['id_barang'];
                                         ?>
-
                                         <option value="<?=$idbarang;?>"><?=$jenisbarang;?></option>
-
                                         <?php
                                             }
 
@@ -107,112 +106,125 @@ require 'function.php';
 <!-- Tabel  --> 
     				<div class="card-body">
     					<div class="table-responsive-xxl">
-    						<table class="table table-bordered overflow-scroll" id="dataTable" width="100%">
-    							<thead>
-    								<tr>
-    									<th style="width: 5%;">No</th>
-    									<th style="width: 10%;">Id Barang</th>
-    									<th style="width: 15%;">Tanggal</th>
-    									<th style="width: 30%;">Nama Barang</th>
-    									<th style="width: 5%;">Stok</th>
-    									<th style="width: 15%;">Supplier</th>
-    									<th style="width: 20%;">Aksi</th>
-    								</tr>
-    							</thead>
-    							<tbody>
-                                   <?php
-                                   $i = 1;
-                                    $getdata = mysqli_query($koneksi, "SELECT * FROM masuk m, stok = s where s.id_barang = m.id_barang");
-                                    while($row=mysqli_fetch_array($getdata)){
-                                        $id_masuk = $row['id_masuk'];
-                                        $id_barang = $row['id_barang'];
-                                        $tanggal = $row['tanggal'];
-                                        $nama_barang = $row['nama_barang'];
-                                        $quantitas = $row['quantitas'];
-                                        $supplier = $row['supplier'];
-                                    ?>
-                                    <tr>
-                                        <td><?=$i;?></td>
-                                        <td><?=$id_barang;?></td>
-                                        <td><?= date("d F Y", strtotime($tanggal));?></td> 
-                                        <td><?=$nama_barang;?></td>
-                                        <td><?=$quantitas;?></td>
-                                        <td><?=$supplier;?></td>
-                                        <td>
-                                            <!-- <a class="btn btn-warning"><i class="far fa-edit"></i></a>
-                                            <a class="btn btn-danger"><i class="far fa-trash-alt"></i></a> -->
+                            <div id="dataTable_wrapper" class="dataTable_wrapper">
+                                <div class="row">
+                                    <div class="col-sm-12 col-md-6">
+                                        <a type="button" target="_blank" href="cetakmasuk.php" class="btn btn-secondary">Cetak Laporan</a>
+                                    </div>
+                                    <div class="col-sm-12 col-md-6">
+                                        <div id="dataTable_filter" class="dataTable_filter">
+                                            <div class="container-fluid" style="max-width: 400px;">
+                                            <form action="<?php echo $_SERVER["PHP_SELF"];?>" class="d-flex" method="GET">
+                                                <input class="form-control me-2" type="search" name="cari" placeholder="Search" aria-label="Search">
+                                                <button class="btn btn-outline-success" type="submit">Search</button>
+                                            </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
 
-                                            <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#edit<?=$id_masuk;?>"><i class="far fa-edit"></i> Edit </button>
-                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete<?=$id_masuk;?>"><i class="far fa-trash-alt"></i> Hapus </button>
-                                        </td>
-<!-- Modal Edit Barang -->
-                                        <form action="" method="POST">
-                                        <div class="modal fade" id="edit<?=$id_masuk;?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                            <div class="modal-dialog">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <h5 class="modal-title" id="exampleModalLabel">Edit Data Barang Masuk</h5>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        <input class="form-control" type="text" name="id_barang" value="<?=$id_barang;?>" aria-label="readonly input example" readonly><br>
-                                                        <input class="form-control" type="text" name="nama_barang" value="<?=$nama_barang;?>" aria-label="readonly input example" readonly><br>
-                                                        <input type="text" name="quantitas" value="<?=$quantitas;?>" class="form-control" required><br>
-                                                        <input type="text" name="supplier" value="<?=$supplier;?>" class="form-control" required><br>
-                                                        <input type="hidden" name="id_barang" value="<?=$id_barang;?>">
-                                                        <input type="hidden" name="id_masuk" value="<?=$id_masuk;?>">
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="Submit" class="btn btn-primary" name="editmasuk">Submit</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        </form>
-<!-- Modal Hapus Barang -->
-                                        <form action="" method="POST">
-                                        <div class="modal fade" id="delete<?=$id_masuk;?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                            <div class="modal-dialog">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalLabel">Hapus Data Barang</h5>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        Apakah Anda yakin ingin menghapus data <?=$nama_barang;?>
-                                                        <input type="hidden" name="id_barang" value="<?=$id_barang;?>">
-                                                        <input type="hidden" name="quantitas" value="<?=$quantitas;?>">
-                                                        <input type="hidden" name="id_masuk" value="<?=$id_masuk;?>">
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="Submit" class="btn btn-danger" name="hapusbarangmasuk">Hapus</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        </form>
-                                    </tr>
-                                    <?php $i++; ?>
-                                    <?php
-                                    };
+                                <div class="row">
+                                    <div class="col-sm-12 mt-3">
+                                    <table class="table table-bordered dataTable no-footer overflow-scroll" id="dataTable" width="100%">
+                                        <thead>
+                                            <tr>
+                                                <th style="width: 5%;">No</th>
+                                                <th style="width: 15%;">Id Barang</th>
+                                                <th style="width: 15%;">Tanggal</th>
+                                                <th style="width: 25%;">Nama Barang</th>
+                                                <th style="width: 5%;">Stok</th>
+                                                <th style="width: 15%;">Supplier</th>
+                                                <th style="width: 20%;">Aksi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                           <?php
+                                           $i = 1;
                                             
-                                    ?>
-                                </tbody>
-    						</table>
+                                            if (isset($_GET['cari'])) {
+                                                $cari = $_GET['cari'];
+                                                $tgl = date("Y-m-d", strtotime($cari));
+                                                $getdata = mysqli_query($koneksi, "SELECT id_masuk, s.id_barang, m.tgl_masuk, s.nama_barang, m.supplier, m.quantitas FROM masuk m, stok s WHERE s.id_barang = m.id_barang AND (s.id_barang like '%".$cari."%' OR m.tgl_masuk like '%".$tgl."%' OR s.nama_barang like '%".$cari."%' OR m.supplier like '%".$cari."%' OR m.quantitas like '%".$cari."%')");
+                                            } else {
+                                                $getdata = mysqli_query($koneksi, "SELECT * FROM masuk m, stok s where s.id_barang = m.id_barang ORDER BY m.tgl_masuk DESC");
+                                            }
+                                            //$data = mysqli_query($koneksi, "SELECT s.id_barang, tgl_masuk, nama_barang, quantitas, m.supplier FROM masuk m, stok s where s.id_barang = m.id_barang ORDER BY m.tgl_masuk DESC");
+                                            while($row=mysqli_fetch_array($getdata)){
+                                                $id_barang = $row['id_barang'];
+                                                $tanggal = $row['tgl_masuk'];
+                                                $nama_barang = $row['nama_barang'];
+                                                $quantitas = $row['quantitas'];
+                                                $supplier = $row['supplier'];
+                                            ?>
+                                            <tr>
+                                                <td><?=$i;?></td>
+                                                <td><?=$id_barang;?></td>
+                                                <td><?= date("d F Y", strtotime($tanggal));?></td> 
+                                                <td><?=$nama_barang;?></td>
+                                                <td><?=$quantitas;?></td>
+                                                <td><?=$supplier;?></td>
+                                                <td>
+                                                    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#edit<?=$id_masuk;?>" style="width: 100px";><i class="far fa-edit"></i> Edit </button><br>
+                                                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete<?=$id_masuk;?>" style="width: 100px";><i class="far fa-trash-alt"></i> Hapus </button>
+                                                </td>
+        <!-- Modal Edit Barang -->
+                                                <form action="" method="POST">
+                                                <div class="modal fade" id="edit<?=$id_masuk;?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="exampleModalLabel">Edit Data Barang Masuk</h5>
+                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <input class="form-control" type="text" name="id_barang" value="<?=$id_barang;?>" aria-label="readonly input example" readonly><br>
+                                                                <input class="form-control" type="text" name="nama_barang" value="<?=$nama_barang;?>" aria-label="readonly input example" readonly><br>
+                                                                <input type="text" name="quantitas" value="<?=$quantitas;?>" class="form-control" required><br>
+                                                                <input type="text" name="supplier" value="<?=$supplier;?>" class="form-control" required><br>
+                                                                <input type="hidden" name="id_barang" value="<?=$id_barang;?>">
+                                                                <input type="hidden" name="id_masuk" value="<?=$id_masuk;?>">
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="Submit" class="btn btn-primary" name="editmasuk">Submit</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                </form>
+        <!-- Modal Hapus Barang -->
+                                                <form action="" method="POST">
+                                                <div class="modal fade" id="delete<?=$id_masuk;?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                            <h5 class="modal-title" id="exampleModalLabel">Hapus Data Barang</h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                Apakah Anda yakin ingin menghapus data <?=$nama_barang;?>
+                                                                <input type="hidden" name="id_barang" value="<?=$id_barang;?>">
+                                                                <input type="hidden" name="quantitas" value="<?=$quantitas;?>">
+                                                                <input type="hidden" name="id_masuk" value="<?=$id_masuk;?>">
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="Submit" class="btn btn-danger" name="hapusbarangmasuk">Hapus</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                </form>
+                                            </tr>
+                                            <?php $i++; ?>
+                                            <?php
+                                            };
+                                                    
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                    </div>
+                                </div>
+                            </div>
     					</div>
-    					<!-- <nav aria-label="Page navigation example">
-						  <ul class="pagination justify-content-end">
-						    <li class="page-item disabled">
-						      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-						    </li>
-						    <li class="page-item"><a class="page-link" href="#">1</a></li>
-						    <li class="page-item"><a class="page-link" href="#">2</a></li>
-						    <li class="page-item"><a class="page-link" href="#">3</a></li>
-						    <li class="page-item">
-						      <a class="page-link" href="#">Next</a>
-						    </li>
-						  </ul>
-						</nav> -->
     				</div>
     			</div>
     		</div>
